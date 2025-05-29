57c363a271b32e816bdc7b51ea0af4aeb0f1d722
Emanuel-DevX <jefymoxi@gmail.com>
2025-05-28 22:35:10 -0400

    Add healthArticles.js to handle basic fetch using xhr

---
+++ healthArticle/healthArticle.js
@@ -0,0 +1,8 @@
+var xhr = new XMLHttpRequest();
+var url = './health_article.json'
+xhr.open('GET', url, true)
+xhr.responseType = 'json'
+
+var articles = xhr.response.articles
+var articlesDiv = document.getElementById("articles")
+console.log(articles)
