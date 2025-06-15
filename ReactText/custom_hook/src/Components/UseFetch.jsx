22647b19cd7ff6e205d522a41ae8e691679dcc4a
Emanuel-DevX <jefymoxi@gmail.com>
2025-06-14 22:19:24 -0700

    Create a custom fetch hook

--- ReactText/custom_hook/src/Components/UseFetch.jsx
+++ ReactText/custom_hook/src/Components/UseFetch.jsx
@@ -1,5 +1,14 @@
+import { useEffect, useState } from "react";
 const UseFetch = (url) => {
-    
-}
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch(url)
+      .then(resizeBy.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+  return [data]
+};
 
-export default UseFetch
+export default UseFetch;
